eeef63c0d45fa64837d995f8adc50aaf7f4c3d6f
Gnoah <root@localhost.localdomain>
2019-03-06 08:10:04 +0300

    okey

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './component/Acceuil/Aceuil';
+import About from './component/Apropos/apropos';
+import Contact from './component/contact/contact';
+import './App.css';
+
+import './component/Acceuil/acceuil.css';
+import './component/Apropos/apropos.css';
+import './component/contact/contact.css';
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+           <div>
+              <div class="body">
+                <nav>
+                  <ul>
+                    <li>
+                      <Link className="col" to="/acceuil">Acceuil</Link>
+                    </li>
+                    <li>
+                      <Link className="col" to="/about">Apropos</Link>
+                    </li>
+                    <li>
+                      <Link className="col" to="/contact">Contact</Link>
+                    </li>
+                  </ul>
+                </nav>
+
+                <Route path="/" exact component={Home} />
+                <Route path="/acceuil" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+              </div>
+                <footer>footer</footer>              
+              </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
